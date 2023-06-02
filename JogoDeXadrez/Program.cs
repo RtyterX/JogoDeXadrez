2f73c766fbeaa6d5c8828de1cbe5e5bd47c70ebb
Ricardo Thiago Firmino <ricardothiagofirmino@hotmail.com>
2023-06-01 21:25:11 -0300

    Teste de Erros (Posicão das Pecas)

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -8,17 +8,24 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8,8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Dama(tab, Cor.Preto), new Posicao(0, 3));
-            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 1));
-            tab.colocarPeca(new Cavalo(tab, Cor.Preto), new Posicao(2, 7));
-            tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(3, 5));
-            tab.colocarPeca(new Peao(tab, Cor.Preto), new Posicao(6, 7));
+                tab.colocarPeca(new Dama(tab, Cor.Preto), new Posicao(0, 3));
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 1));
+                tab.colocarPeca(new Cavalo(tab, Cor.Preto), new Posicao(2, 7));
+                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(3, 5));
+                tab.colocarPeca(new Peao(tab, Cor.Preto), new Posicao(6,7));
 
-            
+                Tela.imprimirTabuleiro(tab);
 
-            Tela.imprimirTabuleiro(tab);
+            }
+
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
 
